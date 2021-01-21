bf3825e1f3f4a3ccbe51eb561e355fc366a7226e
arbeitstein <philipp.arbeitstein@gmail.com>
2021-01-21 19:39:50 +0100

    Get Request Example

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-
-
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
 from sqlalchemy.ext.automap import automap_base
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@192.168.1.100:5432/Raspberry Database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@127.0.0.1:5432/SmartFlow'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 Base = automap_base()
 Base.prepare(db.engine, reflect = True)
-User_settings = Base.classes.user_settings
+sf_user = Base.classes.sf_user
 
 @app.route('/')
+def base():
+    return "Hello World 1"
+
+
+@app.route('/get', methods=['GET'])
 def index():
-    results = db.session.query(User_settings).all()
-    for r in results:
-        print(str(r.area_of_solarpanel)) 
-    return 'Hello World'
+    if request.method == 'GET':
+        results = db.session.query(sf_user).all()
+        for r in results:
+            print(str(r.firstname))
+    return "Hello World 2 - GET"
 
 if __name__ == "__main__":
     app.run(debug = True)
