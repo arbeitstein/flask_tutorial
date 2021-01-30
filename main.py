a10764f08a34bac5d0ba6713f50e66b6e91bd8f2
Philipp <philipp.arbeitstein@gmail.com>
2021-01-30 15:42:59 +0100

    main.py changes in website

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
 from flask_marshmallow import Marshmallow
-from collections import OrderedDict
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@192.168.1.100:5432/Raspberry Database'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSON_SORT_KEYS'] = False
 
 ma = Marshmallow(app)
 db = SQLAlchemy(app)
@@ -27,32 +25,16 @@ users_schema = UserSchema(many=True)
 
 @app.route('/')
 def base():
-    return "base"
+    return "Hello, this is the Standard Website, you get prompted to when opening the link in the console."
 
-@app.route('/settings', methods=['GET'])
+@app.route('/user', methods=['GET'])
 def index_get():
     if request.method == 'GET':
         results = db.session.query(sf_user).all()
         data = users_schema.dump(results)
-        for r in data:
-            dict_users = {
-                "data": {
-                    "r":{
-                    "id": r['id'],
-                    "firstname": r['firstname'],
-                    "email": r['email'],
-                    "password": r['password'],
-                    "settings_id": r['settings_id'],
-                    "shelly_id": r['shelly_id'],
-                    "weather_id": r['weather_id'],
-                    "measurement_id": r['measurement_id']
-                    }
-                }
-            }
-            print(dict_users)
-    return jsonify(dict_users)
+    return jsonify(data)
 
-@app.route('/settings/<first>', methods=['POST'])
+@app.route('/post/<first>', methods=['POST'])
 def index_post(first):
     if request.method == 'POST':
         new_setting = user_settings(solarpanel=first)
